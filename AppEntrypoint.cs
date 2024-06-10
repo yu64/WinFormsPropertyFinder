d1992020bb92fd9ba9ef4217fdc39670f34e04d5
yu64 <yuma1212y@gmail.com>
2024-06-10 13:05:03 +0900

    引数のパースが可能か確認

---
+++ AppEntrypoint.cs
@@ -0,0 +1,44 @@
+﻿using System.CommandLine;
+using System.Security.Cryptography;
+using WinFormsPropertyFinder.action;
+
+namespace WinFormsPropertyFinder;
+
+/// <summary>
+/// Main関数と同名のクラスを作成できないため、この名称。
+/// </summary>
+class AppEntrypoint
+{
+    public static async Task<int> Main(string[] args)
+    {
+        RootCommand cmd = new()
+        {
+            Description = "Visual Studioの WinFormsプロパティペインからプロパティを検索します"
+        };
+
+        cmd.AddCommand(AppEntrypoint.CreateProcessCommand());
+        
+
+        return await cmd.InvokeAsync(args);
+    }
+
+
+    private static Command CreateProcessCommand()
+    {
+        Command cmd = new Command("process", "");
+
+        Option<string> configFileOption = new Option<string>(
+            aliases: new string[] {"--list", "-l"},
+            description: "プロセスリスト"
+        );
+
+        cmd.AddOption(configFileOption);
+
+        cmd.SetHandler<string>(ProcessListDetector.GetProcessList, configFileOption);
+
+        return cmd;
+    }
+
+
+
+}
---
+++ action/ProcessListDetector.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WinFormsPropertyFinder.action
+{
+    public class ProcessListDetector
+    {
+        public static void GetProcessList(string list)
+        {
+            System.Console.WriteLine($"Hello! {list}");
+        }
+    }
+}
